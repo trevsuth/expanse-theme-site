e9a2449cfed4d3cabf0e61f371c9608a69f99d36
trevsuth <trevor.sutherland@gmail.com>
2024-01-29 16:45:30 -0500

    added sidebar slideout

--- components/SidebarComponent.tsx
+++ components/SidebarComponent.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react';
 import SineWaveComponent from './SineWaveComponent';
 
 const SidebarComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="sidebar">
-      <SineWaveComponent text="Page 1" />
-      <SineWaveComponent text="Page 2" />
-      <SineWaveComponent text="Page 3" />
-      <SineWaveComponent text="Page 4" />
-    </div>
+    <>
+      <button onClick={toggleSidebar} className="sidebar-toggle">
+        {isOpen ? 'Hide' : 'Show'} Sidebar
+      </button>
+      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+        <SineWaveComponent text="Page 1" />
+        <SineWaveComponent text="Page 2" />
+        <SineWaveComponent text="Page 3" />
+        <SineWaveComponent text="Page 4" />
+      </div>
+    </>
   );
 };
 
