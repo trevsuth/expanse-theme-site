2bd274d27a939fdcfbcffc59f3045d9a6438e907
trevsuth <trevor.sutherland@gmail.com>
2024-01-29 22:08:14 -0500

    added slide in for keyboard

--- components/OnScreenKeyboard.tsx
+++ components/OnScreenKeyboard.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client'
+
+import React, { useState, useEffect } from 'react';
 
 const keysRow0 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 const keysRow1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
@@ -10,8 +12,28 @@ const Key = ({ value }) => (
 );
 
 const OnScreenKeyboard = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleKeyboard = () => {
+    setIsVisible(!isVisible);
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (e: { key: string; preventDefault: () => void; }) => {
+      if (e.key === 'v' || e.key === 'V') {
+        e.preventDefault();
+        toggleKeyboard();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [isVisible]);
   return (
-    <div className="keyboard">
+    <div className={`keyboard ${isVisible ? 'visible' : ''}`}>
       <div className="keys-row">
         {keysRow0.map(key => <Key key={key} value={key} />)}
       </div>
