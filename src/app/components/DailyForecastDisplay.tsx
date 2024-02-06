1bef5d62849014ee3d47680e32854bbd074faaa8
trevsuth <trevor.sutherland@gmail.com>
2024-02-05 22:13:38 -0500

    make hourly component

---
+++ src/app/components/DailyForecastDisplay.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import getStationInfo from '../utils/getStationInfo';
+import VisualizeDailyForecast from './VisualizeDailyForecast';
+import getCurrentLocation from '../utils/getCurrentLocation';
+
+const DailyForecastPage = () => {
+    const [forecastUrl, setForecastUrl] = useState<string>('');
+
+    useEffect(() => {
+        const fetchForecastUrl = async () => {
+            try {
+                const location = await getCurrentLocation();
+                if (location) {
+                    const { latitude, longitude } = location;
+                    const stationInfo = await getStationInfo(latitude, longitude);
+                    if (stationInfo && stationInfo.forecastDailyUrl) {
+                        setForecastUrl(stationInfo.forecastDailyUrl);
+                    } else {
+                        console.error('No forecast URL found');
+                    }
+                }
+            } catch (error) {
+                console.error('Error fetching forecast URL:', error);
+            }
+        };
+
+        fetchForecastUrl();
+    }, []);
+
+    return (
+        <div>
+            <h1>Daily Forecast</h1>
+            {forecastUrl ? (
+                <VisualizeDailyForecast forecastUrl={forecastUrl} />
+            ) : (
+                <p>Loading forecast information...</p>
+            )}
+        </div>
+    );
+};
+
+export default DailyForecastPage;
---
+++ src/app/components/DisplayHourlyForecast.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import getStationInfo from '../utils/getStationInfo';
+import VisualizeHourlyForecast from './VisualizeHourlyForecast';
+import getCurrentLocation from '../utils/getCurrentLocation';
+
+const HourlyForecastPage = () => {
+  const [forecastUrl, setForecastUrl] = useState<string>('');
+
+  useEffect(() => {
+    const fetchForecastUrl = async () => {
+      try {
+        const location = await getCurrentLocation();
+        if (location) {
+          const { latitude, longitude } = location;
+          const stationInfo = await getStationInfo(latitude, longitude);
+          if (stationInfo && stationInfo.forecastDailyUrl) {
+            setForecastUrl(stationInfo.forecastDailyUrl);
+          } else {
+            console.error('No forecast URL found');
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching forecast URL:', error);
+      }
+    };
+
+    fetchForecastUrl();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hourly Forecast</h1>
+      {forecastUrl ? (
+        <VisualizeHourlyForecast forecastUrl={forecastUrl} />
+      ) : (
+        <p>Loading forecast information...</p>
+      )}
+    </div>
+  );
+};
+
+export default HourlyForecastPage;
